aa176cfaae6c9c126c60f169312cf7fed4133ef6
QuangMinhNguyen27405 <quangminh27405@gmail.com>
2024-10-06 23:20:54 -0400

    feature: add experience

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
     <main className='main'>
       <Home />
       <About />
+      <Skills />
+      <Services />  
+      
     </main>
     </>
   )
---
+++ personal-portfolio/src/components/skills/Backend.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Backend Skills</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
+                        <h3 className="skills__name">C++</h3>
+                        <span className="skills__level">Class Project Level</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
+                        <h3 className="skills__name">Java/Spring</h3>
+                        <span className="skills__level">Internship + Personal Project level</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
+                        <h3 className="skills__name">Python</h3>
+                        <span className="skills__level">Personal Project + Research level</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+                    <div>
+                        <h3 className="skills__name">MySQL</h3>
+                        <span className="skills__level">Internship + Personal Project level</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend
